43c5f42d9353d394511f662194babea0adc14e55
pakitowka <whitesaint12@gmail.com>
2023-07-29 16:06:38 +0200

    ready, many bugs but objective was to try kivy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import urllib
+
 
 
 Builder.load_file('frontend.kv')
@@ -9,7 +12,17 @@ Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
 
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'images/pic.jpg'
+        # get user input
+        query = self.manager.current_screen.ids.user_query.text
+        # get wikipedia page and first image url
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        image_path = "images/image.jpg"
+        # download the image
+        urllib.request.urlretrieve(image_link,image_path)
+        # set image in widget
+        self.manager.current_screen.ids.img.source = image_path
+        self.ids.img.reload()
 
 
 class RootWidget(ScreenManager):
